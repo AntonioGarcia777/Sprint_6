9d02431461df7b6d9e3b12fd0181fa2f7ab3d5e0
Antonio Garcia Ruiz - Angular <antoniogarcia.ruizdiaz@gmail.com>
2022-06-11 16:21:11 +0200

    ejercicio 3

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.css']
+  styleUrls: ['./escena.component.css']  
 })
 export class EscenaComponent implements OnInit { 
 
