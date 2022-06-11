e7a7cebabbf99e91410e52e5ac89bf71a4bde1d4
Antonio Garcia Ruiz - Angular <antoniogarcia.ruizdiaz@gmail.com>
2022-06-11 11:25:31 +0200

    ejercicio-2

---
+++ src/app/escena/escena.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escena',
+  templateUrl: './escena.component.html',
+  styleUrls: ['./escena.component.css']
+})
+export class EscenaComponent implements OnInit { 
+
+  @Input () fraseDeTextos:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+/*import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];*/
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  
+  textos: any[];
+
+  constructor() {
+
+    this.textos= [
+
+      {texto:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"},
+      {texto:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."},
+      {texto:"L'heroi va decidir travessar la porta que el portava a casa"},
+      {texto:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."},
+
+    ]
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
